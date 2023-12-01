eec04e1d286f1c55d58935d532d0fcf7ce0f2c97
PSacfc <59246149+PSacfc@users.noreply.github.com>
2023-12-01 16:47:37 +0800

    Update M4.sh

--- Short-term_Forecasting/scripts/M4.sh
+++ Short-term_Forecasting/scripts/M4.sh
@@ -1,51 +1,51 @@
 model_name=GPT4TS
 
-# python -u run.py \
-#   --task_name short_term_forecast \
-#   --is_training 1 \
-#   --root_path ./dataset/m4 \
-#   --seasonal_patterns 'Monthly' \
-#   --model_id m4_Monthly \
-#   --model $model_name \
-#   --data m4 \
-#   --features M \
-#   --enc_in 1 \
-#   --dec_in 1 \
-#   --c_out 1 \
-#   --gpt_layer 6 \
-#   --d_ff 128 \
-#   --d_model 128 \
-#   --patch_size 1 \
-#   --stride 1 \
-#   --batch_size 16 \
-#   --des 'Exp' \
-#   --itr 1 \
-#   --learning_rate 0.002 \
-#   --loss 'SMAPE'
+python -u run.py \
+  --task_name short_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/m4 \
+  --seasonal_patterns 'Monthly' \
+  --model_id m4_Monthly \
+  --model $model_name \
+  --data m4 \
+  --features M \
+  --enc_in 1 \
+  --dec_in 1 \
+  --c_out 1 \
+  --gpt_layer 6 \
+  --d_ff 128 \
+  --d_model 128 \
+  --patch_size 1 \
+  --stride 1 \
+  --batch_size 16 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.002 \
+  --loss 'SMAPE'
 
-# python -u run.py \
-#   --task_name short_term_forecast \
-#   --is_training 1 \
-#   --root_path ./dataset/m4 \
-#   --seasonal_patterns 'Yearly' \
-#   --model_id m4_Yearly \
-#   --model $model_name \
-#   --data m4 \
-#   --features M \
-#   --enc_in 1 \
-#   --dec_in 1 \
-#   --c_out 1 \
-#   --ln 1 \
-#   --gpt_layer 6 \
-#   --d_model 768 \
-#   --d_ff 32 \
-#   --patch_size 1 \
-#   --stride 1 \
-#   --batch_size 16 \
-#   --des 'Exp' \
-#   --itr 1 \
-#   --learning_rate 0.001 \
-#   --loss 'SMAPE'
+python -u run.py \
+  --task_name short_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/m4 \
+  --seasonal_patterns 'Yearly' \
+  --model_id m4_Yearly \
+  --model $model_name \
+  --data m4 \
+  --features M \
+  --enc_in 1 \
+  --dec_in 1 \
+  --c_out 1 \
+  --ln 1 \
+  --gpt_layer 6 \
+  --d_model 768 \
+  --d_ff 32 \
+  --patch_size 1 \
+  --stride 1 \
+  --batch_size 16 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --loss 'SMAPE'
 
 python -u run.py \
   --task_name short_term_forecast \
